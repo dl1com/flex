44a3928aacf13271d295bc24185ced2c4f254904
chris <mail@chris007.de>
2017-11-14 21:53:42 +0100

    putting flex code into lib

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-mod parity;
-mod bch_calculator;
-mod fourbit_checksum;
-mod codeword;
-mod cw_fiw;
-mod cw_biw1;
-mod cw_biw2;
-mod cw_biw3;
-mod cw_biw4;
-mod cw_address_short;
-mod cw_vector_alpha;
-mod cw_message_alpha_header;
-mod cw_message_alpha_signature;
-mod cw_message_alpha_content;
-mod cw_message_alpha;
-mod frame;
-mod blocks;
-mod apply_bch_and_parity;
-mod message;
+extern crate flex;
 
-use frame::Frame;
-use message::Message;
-use message::MessageType;
+use flex::frame::Frame;
+use flex::message::*;
 
 use std::fs::File;
 use std::io::prelude::*;
