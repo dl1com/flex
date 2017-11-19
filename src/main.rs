550122d20673547f3ddc0e461f41a8ca280ca8ee
chris <mail@chris007.de>
2017-11-19 23:19:03 +0100

    implemented continuous sending mode over UDP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::time;
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+extern crate chrono;
+use chrono::prelude::*;
+
 extern crate flex;
 use flex::frame::Frame;
 use flex::message::*;
@@ -15,6 +22,8 @@ use bit_reverse::ParallelReverse;
 extern crate serde;
 extern crate serde_json;
 
+use std::net::UdpSocket;
+
 use std::process::exit;
 
 const CYCLES_PER_HOUR : u32 = 15;
@@ -22,7 +31,8 @@ const FRAMES_PER_CYCLE: u32 = 128;
 
 enum OperationMode {
     Single,
-    Hour
+    Hour,
+    Continuous
 }
 
 fn print_usage(program: &str, opts: Options) {
@@ -38,6 +48,7 @@ fn parse_arguments(args: &Vec<String>) -> Result<(String,String,OperationMode),&
     opts.optopt("o", "output","set output file (FLEX bytestream)", "FILE");
     opts.optflag("", "single","MODE: generate only frames for the messages given");
     opts.optflag("", "hour","MODE: generate a full hour of frames");
+    opts.optflag("", "continuous","MODE: permanently generating frames");
     opts.optflag("h", "help", "print this help menu");    
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -54,10 +65,9 @@ fn parse_arguments(args: &Vec<String>) -> Result<(String,String,OperationMode),&
         None    => return Err("No input file given."),
     }
 
-    let out_file;
-    match matches.opt_str("o") {
-        Some(x) => out_file = x,
-        None    => return Err("No output file given."),
+    let mut out_file = "/dev/null".to_string();
+    if matches.opt_present("o") {
+        out_file = matches.opt_str("o").unwrap();
     }
 
     let mode: OperationMode;
@@ -67,6 +77,9 @@ fn parse_arguments(args: &Vec<String>) -> Result<(String,String,OperationMode),&
     else if matches.opt_present("single") {
         mode = OperationMode::Single;
     }
+    else if matches.opt_present("continuous") {
+        mode = OperationMode::Continuous;
+    }
     else
     {
         return Err("No operation mode given.")
@@ -75,7 +88,10 @@ fn parse_arguments(args: &Vec<String>) -> Result<(String,String,OperationMode),&
     return Ok((in_file,out_file,mode));
 }
 
-fn generate_hour(msg_vec: Vec<Message>) -> Vec<Frame> {
+fn generate_hour(in_file: String, out_file: String) {
+    let file = File::open(in_file).unwrap();
+    let msg_vec: Vec<Message> = serde_json::from_reader(file).unwrap();
+
     let mut frames = Vec::new();        
     for cycle_nr in 0..CYCLES_PER_HOUR {
         for frame_nr in 0..FRAMES_PER_CYCLE {
@@ -89,10 +105,13 @@ fn generate_hour(msg_vec: Vec<Message>) -> Vec<Frame> {
             frames.push(frame);
         }
     }
-    return frames;
+    write_frames_to_file(&frames, &out_file);
 }
 
-fn generate_single_frames(msg_vec: Vec<Message>) -> Vec<Frame> {
+fn generate_single_frames(in_file: String, out_file: String) {
+    let file = File::open(in_file).unwrap();
+    let msg_vec: Vec<Message> = serde_json::from_reader(file).unwrap();
+
     let mut frames = Vec::new();
     for msg in &msg_vec {
         let mut frame = Frame::new(0, msg.frame).unwrap();
@@ -100,7 +119,70 @@ fn generate_single_frames(msg_vec: Vec<Message>) -> Vec<Frame> {
         frames.push(frame);
         println!("added {:?}", msg);
     }    
-    return frames;
+    write_frames_to_file(&frames, &out_file);
+}
+
+fn write_frames_to_file(frames: &Vec<Frame>, filename: &String) {
+    let mut file = File::create(filename).unwrap();      
+    file.write_all(&frames_hton(frames)).unwrap();
+}
+
+fn frames_hton(frames: &Vec<Frame>) -> Vec<u8> {
+    let mut reversed_bytes = Vec::new();
+    for frame in frames {
+        let bytes = frame.get_bytes();        
+        for byte in bytes {
+            reversed_bytes.push(byte.swap_bits());
+        }
+    }
+    return reversed_bytes;
+}
+
+fn generate_continuousely(in_file: String) {
+    let share_msg_input_0 = Arc::new(Mutex::new(Vec::new()));
+    let share_msg_input_1 = share_msg_input_0.clone();
+
+    let input_thread = thread::spawn(move || {
+        loop {
+            let file = File::open(&in_file).unwrap();
+            let mut msg_vec: Vec<Message> = serde_json::from_reader(file).unwrap();
+            
+            println!("Queuing message(s): {:?}", msg_vec);
+            let mut shared = share_msg_input_0.lock().unwrap();
+            shared.append(&mut msg_vec);            
+        }
+    });
+
+    let socket = UdpSocket::bind("127.0.0.1:34254").expect("couldn't bind to address");
+
+    let mut msg_vec: Vec<Message> = Vec::new();
+    let encoder_thread = thread::spawn(move || {
+        loop {
+            msg_vec.append(&mut share_msg_input_1.lock().unwrap());
+
+            let dt = Utc::now();
+            let (cycle_nr,frame_nr) = Frame::calculate_cycle_and_frame(dt.minute(), dt.second());
+            println!("Time: {}:{}, Cycle: {}, Frame: {} - Size of MsgQueue: {}",
+                     dt.minute(), dt.second(), cycle_nr, frame_nr, msg_vec.len());
+            
+            let mut frame = Frame::new(cycle_nr, frame_nr).unwrap();
+            for i in 0..msg_vec.len() {
+                if msg_vec[i].frame == frame_nr{
+                    if msg_vec[i].get_num_of_message_codewords().unwrap() < frame.space_left() {
+                        frame.add_message(&msg_vec[i]).unwrap();
+                        msg_vec.remove(i);
+                    }
+                }
+            }
+
+            socket.send_to(&frames_hton(&vec![frame]), "127.0.0.1:51337").expect("couldn't send data");
+
+            thread::sleep(time::Duration::from_millis(1875));
+        }
+    });
+
+    let _ = input_thread.join();
+    let _ = encoder_thread.join();
 }
 
 fn main() {
@@ -108,22 +190,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let (in_file,out_file,mode) = parse_arguments(&args).unwrap();
     
-    let file = File::open(in_file).unwrap();
-    let msg_vec: Vec<Message> = serde_json::from_reader(file).unwrap();
-
-    let frames;
     match mode {
-        OperationMode::Single => frames = generate_single_frames(msg_vec),
-        OperationMode::Hour => frames = generate_hour(msg_vec)
-    }
-
-    let mut file = File::create(out_file).unwrap();
-    for frame in frames {
-        let bytes = frame.get_bytes();
-        let mut rotated_bytes = Vec::new();
-        for byte in bytes {
-            rotated_bytes.push(byte.swap_bits());
-        }        
-        file.write_all(&rotated_bytes).unwrap();
+        OperationMode::Single => generate_single_frames(in_file, out_file),
+        OperationMode::Hour => generate_hour(in_file, out_file),
+        OperationMode::Continuous => generate_continuousely(in_file),
     }
 }
